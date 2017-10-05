71bbd6d0806f56d36ae4d4e66a94c9ffd46e35a7
ngodfraind <nicolas.godfraind@gmail.com>
2017-10-05 14:30:17 +0200

    Oauth

---
+++ src/Request/Entity/Oauth.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\API\Request\Entity;
+
+use Claroline\API\Request\AbstractQuery;
+use Claroline\API\Request\Model\HasOrganization;
+use Claroline\API\Request\Model\HasRole;
+
+class Oauth extends AbstractQuery
+{
+    public function properties()
+    {
+        return [
+          'id',
+          'oauthId',
+          'service',
+          'user' //any identifier will do
+        ];
+    }
+}
